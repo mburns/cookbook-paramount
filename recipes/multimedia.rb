7baca49f5cb21a60b70e81fdb0e9460d76625204
Michael Burns <michael@mirwin.net>
2017-04-29 23:22:14 -0700

    disable stuff

--- recipes/multimedia.rb
+++ recipes/multimedia.rb
@@ -10,7 +10,7 @@ node.normal['plexapp']['version'] = '1.5.5.3634-995f1dead'
 
 # node.normal['plexapp']['package_url'] = 'https://downloads.plex.tv/plex-media-server/1.5.5.3634-995f1dead/plexmediaserver_1.5.5.3634-995f1dead_amd64.deb'
 
-include_recipe 'plexapp'
+# include_recipe 'plexapp'
 
 # include_recipe 'couchpotato'
 
