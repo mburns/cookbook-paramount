0ff5d1eb54a1a58095a95067ca33f6a19956b483
Michael Burns <michael@mirwin.net>
2017-04-29 21:23:00 -0700

    fix plex deb download URL via setting a modern version

--- recipes/multimedia.rb
+++ recipes/multimedia.rb
@@ -6,6 +6,10 @@
 # License:: Apache License, Version 2.0
 #
 
+node.normal['plexapp']['version'] = '1.5.5.3634-995f1dead'
+
+# node.normal['plexapp']['package_url'] = 'https://downloads.plex.tv/plex-media-server/1.5.5.3634-995f1dead/plexmediaserver_1.5.5.3634-995f1dead_amd64.deb'
+
 include_recipe 'plexapp'
 
 # include_recipe 'couchpotato'
