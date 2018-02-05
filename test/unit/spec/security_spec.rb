44c63ac3d582134be3d17d4e3be55de322afff4c
Michael Burns <michael@mirwin.net>
2018-02-05 03:28:27 -0800

    remove useless unit test

--- test/unit/spec/security_spec.rb
+++ test/unit/spec/security_spec.rb
@@ -11,10 +11,6 @@ describe 'paramount::security' do
 
   cached(:chef_run) { ChefSpec::ServerRunner.new.converge(described_recipe) }
 
-  it 'creates ssl directory with an explicit action' do
-    expect(chef_run).to create_directory('/etc/nginx/ssl')
-  end
-
   # selinux_state[SELinux Permissive]
   # openssl_x509[/etc/nginx/ssl/example.com.pem]
 end
