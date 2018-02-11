17fb55c92a9646d440cc0f888288afdb85e7a064
Michael Burns <michael@mirwin.net>
2018-02-11 00:35:21 -0800

    enable maildrop

--- attributes/postfix.rb
+++ attributes/postfix.rb
@@ -29,7 +29,7 @@ default['postfix']['main']['smtp_sasl_auth_enable'] = 'yes'
 default['postfix']['main']['smtp_sasl_security_options'] = 'keep'
 
 # default['postfix']['master']['cyrus']['active'] = true
-# default['postfix']['master']['maildrop']['active'] = true
+default['postfix']['master']['maildrop']['active'] = true
 
 default['postfix']['sasl']['smtp_sasl_user_name'] = node['paramount']['user']
 
